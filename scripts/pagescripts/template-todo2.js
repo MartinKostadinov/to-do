227801d5307f7d92a74dc4e83eaf1b981df28201
MartinKostadinov <beastdubaka@gmail.com>
2016-11-27 20:55:29 +0200

    general fixes

--- scripts/pagescripts/template-todo2.js
+++ scripts/pagescripts/template-todo2.js
@@ -12,31 +12,8 @@ var templates = (function($) {
 
         return promise;
     }
-//load edit mode template
-    function edit(url) {
-        var promiseEdit = new Promise(function(resolve, reject) {
-            $.get(url, function(addEditFunctionality) {
-                var addEditTemplate = Handlebars.compile(addEditFunctionality);
-                resolve(addEditTemplate);
-            });
-        });
-
-        return promiseEdit;
-    }
-//load  task category template
-    function addTask(url) {
-        var promiseAddTask = new Promise(function(resolve, reject) {
-            $.get(url, function(addTask) {
-                var addTaskCategory = Handlebars.compile(addTask);
-                resolve(addTaskCategory);
-            });
-
-        });
-        return promiseAddTask;
-    }
     return {
         get: get,
-        edit: edit,
-        addTask: addTask
+
     };
 }(jQuery));
--- scripts/pagescripts/todo2-main.js
+++ scripts/pagescripts/todo2-main.js
@@ -13,14 +13,14 @@
             $getTaskColorValue = $container.find('input[name="bg-color"]').val(),
             $getHeaderColorValue = $container.find('input[name="font-color"]').val(),
             $getSelectValue = $container.find('select').val(),
-            url = 'scripts/pagescripts/templates/task-category.handlebars',
+            url = 'scripts/pagescripts/templates/create-task.handlebars',
             addCatInputs = {
                 title: $getTaskTitle,
                 hColor: $getHeaderColorValue,
                 bgColor: $getTaskColorValue
             };
         //load  add task template
-        templates.addTask(url).then(function(addTaskCategory) {
+        templates.get(url).then(function(addTaskCategory) {
             var taskTemplate = addTaskCategory(addCatInputs),
                 $tasksParent = $('main').find('section.row'),
                 $leftSide = $tasksParent.find('.left'),
@@ -46,7 +46,7 @@
 
         var $findButtonParrent = $(this).parents('article'),
             $input = $findButtonParrent.find("input[name='setInput']"),
-            url = 'scripts/pagescripts/templates/to-do-template.handlebars',
+            url = 'scripts/pagescripts/templates/task-template.handlebars',
             $getInputValue = $input.val(),
             $findList = $findButtonParrent.find('ul'),
             inputValue = {
@@ -55,14 +55,13 @@
         //load  Task template
         templates.get(url).then(function(injectValueInHtml) {
             var final = injectValueInHtml(inputValue),
-                //find count of all tasks which have class task-ready
                 totalCount = $findList.children('li').length + 1,
-                readyTskCount = $findList.find('.task-ready').length;
-            //get total tasks count
+                readyTskCount = $findList.find('.task__value--ready-js').length ;
+                 //get total tasks count
             $findList.append(final);
             $input.val('');
-            $findButtonParrent.find('.total').text(totalCount);
-            $findButtonParrent.find('.uncompleted').text(totalCount - readyTskCount);
+            $findButtonParrent.find('.tasks-count__counter--total').text(totalCount);
+            $findButtonParrent.find('.tasks-count__counter--uncompleted').text(totalCount - readyTskCount);
         });
 
     }
@@ -77,7 +76,7 @@
                 text: $taskValue
             };
         //load  edit-task template
-        templates.edit(url).then(function(addEditTemplate) {
+        templates.get(url).then(function(addEditTemplate) {
             var addValueToTemplate = addEditTemplate(textValue);
             $editParent.append(addValueToTemplate);
         });
@@ -99,25 +98,25 @@
             article = ul.parent('article'),
             $readyCount,
             $unReady,
-            $readyTasks = article.find('.completed'),
-            $notReadyTasks = article.find('.uncompleted'),
+            $readyTasks = article.find('.tasks-count__counter--completed'),
+            $notReadyTasks = article.find('.tasks-count__counter--uncompleted'),
             $paragraphValue = $task.find('p'),
             $checkbox = $task.find('input[type="checkbox"]');
         //check if the task has class Ready
-        if (!($this.hasClass('task-ready'))) {
+        if (!($this.hasClass('task__value--ready-js'))) {
             $paragraphValue.css('text-decoration', 'line-through');
             $checkbox.prop('checked', true);
-            $this.removeClass('task-unready').addClass('task-ready');
+            $this.removeClass('task__value--unready-js').addClass('task__value--ready-js');
 
 
         } else {
             $paragraphValue.css('text-decoration', 'none');
             $checkbox.prop('checked', false);
-            $this.removeClass('task-ready').addClass('task-unready');
+            $this.removeClass('task__value--ready-js').addClass('task__value--unready-js');
         }
         //set counter value
-        $readyCount = ul.find('.task-ready').length;
-        $unReady = ul.find('.task-unready').length;
+        $readyCount = ul.find('.task__value--ready-js').length;
+        $unReady = ul.find('.task__value--unready-js').length;
         $readyTasks.text($readyCount);
         $notReadyTasks.text($unReady);
 
@@ -139,7 +138,7 @@
     function deleteTaskCategory() {
         var $this = $(this),
             $deleteTaskParent = $this.parent('article');
-        $deleteTaskParent.addClass('delete-task-cat');
+        $deleteTaskParent.addClass('category--delete-js');
         setTimeout(function() {
             $this.parent('article').remove();
         }, 400);
@@ -156,7 +155,7 @@
     //Function adding animation via CSS to Cancel and Save buttons in edit mode, removing the element from dom
     function animateEditOnAction(parent) {
 
-        parent.removeClass('edit-box').addClass('save-edit');
+        parent.removeClass('edit--show ').addClass('edit--hide');
         setTimeout(function() {
             parent.remove();
         }, 400);
@@ -164,7 +163,7 @@
     }
     //Function restoring  the default state  for Task Button - Edit, when exiting edit mode either viea  Save or Cancel
     function restoreEdit(returnDefaultStateOfEdit) {
-        var $selectEdit = returnDefaultStateOfEdit.find('.edit');
+        var $selectEdit = returnDefaultStateOfEdit.find('.task__btn__edit');
         if ($selectEdit.hasClass('disabled')) {
             $selectEdit.removeClass('disabled');
         }
@@ -175,28 +174,28 @@
             article = $removeTask.parents('article'),
             total, completed, uncompleted;
 
-        $removeTask.addClass('hide-task');
+        $removeTask.addClass('task--hide');
         setTimeout(function() {
             $removeTask.remove();
             //clear counters
             total = article.find('li').length;
-            article.find('.total').text(total);
-            completed = article.find('.task-ready').length;
-            article.find('.completed').text(completed);
-            uncompleted = article.find('.task-unready').length;
-            article.find('.uncompleted').text(uncompleted);
+            article.find('.tasks-count__counter--total').text(total);
+            completed = article.find('.task__value--ready-js').length;
+            article.find('.tasks-count__counter--completed').text(completed);
+            uncompleted = article.find('.task__value--unready-js').length;
+            article.find('.tasks-count__counter--uncompleted').text(uncompleted);
         }, 400);
 
     }
 
     // all binds here
     function binds() {
-        $section.on('click', 'article > .btn-danger', deleteTaskCategory);
-        $('#add-category').on('click', injectAddCatTemplate);
-        $section.on('click', '.btn-success', injectTemplate); //inject task template
-        $section.on('click', 'article .delete', removeTask); //add delete button functionality
-        $section.on('click', ' article .task-value', taskReady);
-        $section.on('click', 'article .edit', editTask); //edit task function
+        $section.on('click', '.category__btn--close ', deleteTaskCategory);
+        $('.btn--add-category').on('click', injectAddCatTemplate);
+        $section.on('click', '.btn__create-task', injectTemplate); //inject task template
+        $section.on('click', '.task__btn__delete', removeTask); //add delete button functionality
+        $section.on('click', ' .task__value', taskReady);
+        $section.on('click', '.task__btn__edit', editTask); //edit task function
         $section.on('click', 'article a.disabled', function(e) { //preventing click multiple clicks on edit button
             e.preventDefault();
         });
